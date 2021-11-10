d3d780318daf6acb3161655ef2fe7f317613f6c2
miguelg1 <miguel.rosa.goncalves@gmail.com>
2021-11-10 18:48:26 -0300

    feat(wip): Add Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,47 @@ import Cart from './components/Cart';
 import Favorites from './components/Favorites';
 import data from "./data";
 
+
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbzwMUBnWUvWfa_M5X3wi90A3zZEn0wpk",
+  authDomain: "front-store-8adc8.firebaseapp.com",
+  projectId: "front-store-8adc8",
+  storageBucket: "front-store-8adc8.appspot.com",
+  messagingSenderId: "604800544353",
+  appId: "1:604800544353:web:1b75da087f681f4f62c420",
+  measurementId: "G-S5Q1LG3548"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db:any) {
+  const citiesCol = collection(db, 'stores');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
+
+async function getProducts(db:any) {
+  const citiesCol = collection(db, 'products');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
+getCities(db).then( test => console.log("test s", test));
+getProducts(db).then( test => console.log("test p", test));
+
+
 const {filters, products, whatsapp} = data;
 ReactDOM.render(
   <React.StrictMode>
